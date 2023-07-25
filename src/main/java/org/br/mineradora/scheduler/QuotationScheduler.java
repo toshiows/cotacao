44c079ce075497f5f63b3d812af473e0809e87f2
toshiows <toshio_ws@hotmail.com>
2023-07-25 00:15:55 -0300

    finishing microservice quotation

--- src/main/java/org/br/mineradora/scheduler/QuotationScheduler.java
+++ src/main/java/org/br/mineradora/scheduler/QuotationScheduler.java
@@ -1,6 +1,8 @@
 package org.br.mineradora.scheduler;
 
 import org.br.mineradora.service.QuotationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.quarkus.scheduler.Scheduled;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -10,12 +12,15 @@ import jakarta.transaction.Transactional;
 @ApplicationScoped
 public class QuotationScheduler {
 
+	private final Logger LOG = LoggerFactory.getLogger(QuotationScheduler.class);
+	
 	@Inject
 	QuotationService quotationService;
 	
 	@Transactional
 	@Scheduled(every = "35s", identity = "task-job")
 	void schedule() {
+		LOG.info("-- Consultando current price --");
 		quotationService.getCurrencyPrice();
 	}
 }
